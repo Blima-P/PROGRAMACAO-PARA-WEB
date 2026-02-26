934f4628f77339cea94dcf7e54da244b5448fa83
Pedro Braga de Lima <p1braga.tech@gmail.com>
2026-02-26 13:52:03 -0300

    Questão02 resolvida

--- Primeira aula/Questão02.js
+++ Primeira aula/Questão02.js
@@ -1,13 +1,26 @@
-document.getElementById('numeroForm').addEventListener('submit', function(event)
-{
-event.preventDefault(); // Impede o envio do formulário
-// Obtém o valor do número digitado
-const numero = parseFloat(document.getElementById('numero').value);
-// Chama a função para calcular o quadrado
-const resultado = calcularQuadrado(numero);
-// Exibe o resultado na página
-document.getElementById('resultado').textContent = resultado;
+document.getElementById('numeroForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Impede o envio do formulário
+    // Obtém o valor do número digitado nos campos
+    const numero = parseFloat(document.getElementById('numero').value);
+    const operacao = document.getElementById('operacao').value;
+    const campoResultado = document.getElementById('resultado');
+
+    let resultado;
+
+    //se a operção for selecionada quadrado o resultado é calculado como quadrado se for cubo calcula como cubo.
+
+    if (operacao === "quadrado") {
+        resultado = calcularQuadrado(numero);
+    } else {
+        resultado = calcularCubo(numero);
+    }
+
+    // resultado mostrado no html
+    campoResultado.textContent = resultado;
 });
-function calcularQuadrado(numero) {
-return numero * numero;
+function calcularCubo(n) {
+    return n * n * n;
+}
+function calcularQuadrado(n) {
+    return n * n;
 }
