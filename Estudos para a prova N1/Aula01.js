f45c5f5426554969970a1a2a92bdd8ed4080559f
Pedro Braga de Lima <pbl0812@gmail.com>
2026-03-23 12:01:48 -0300

    Aula01 e Boas vindas

---
+++ Estudos para a prova N1/Aula01.js
@@ -0,0 +1,5 @@
+console.log("Pedro 'Braga de Lima'"); // String = Texto
+console.log('"Pedro" Braga de Lima'); //Number = Número
+console.log(`"Pedro" 'Braga de Lima'`);
+
+console.log(123456, 15.85,"Pedro Braga de lima");
