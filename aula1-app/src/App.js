92f6b26c7a018d5e4fb1bd867eb5efcd48aa9507
Blima-P <pbl0812@gmail.com>
2026-05-12 12:06:13 -0300

    Introdução ao react

---
+++ aula1-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Mensagem from './components/Mensagem';
+function App() {
+  return (
+    <div className="App">
+      <Mensagem></Mensagem>
+    </div>
+  );
+}
+export default App;
